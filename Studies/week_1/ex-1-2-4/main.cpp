3fdb2c04900fa4469cfece5e14b2eb26d2b439dc
holle1234 <holle1234@github.com>
2023-06-07 15:44:20 +0300

    ex finished

--- Studies/week_1/ex-1-2-4/main.cpp
+++ Studies/week_1/ex-1-2-4/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
-#include <array>
+#include <vector>
+#include <cmath>
 
 
 struct MyTime{
@@ -10,49 +11,74 @@ struct MyTime{
     int second {0};
 
     private:
-        std::string timeDifference(int t1, int t2){
-            return std::to_string(t1 - t2);
+        // timestamp in seconds
+        int getTimeStamp(){
+            int timeStamp {(hour * 3600) + (minute * 60) + second};
+            return timeStamp;
         }
 
     public:
+        // Convert MyTime to int holding seconds passed from 00:00:00
+        // Calculate difference and convert it back to 00:00:00
+
         std::string getTimeDelta(MyTime t2){
-            std::string _delta[3] {};
-    
-            _delta[0] = timeDifference(hour, t2.hour);
-            _delta[1] = timeDifference(minute, t2.minute);
-            _delta[2] = timeDifference(second, t2.second);
+            int timeStampT1 {getTimeStamp()};
+            int timeStampT2 {t2.getTimeStamp()};
+            int timeDelta {std::abs(timeStampT1 - timeStampT2)};
 
-            std::string timeDelta {""};
-            for (int i = 0; i < 3; i++){
-                std::string s = _delta[i];
-
-                if (s.length() < 2){
-                    s = s.insert(0, "0");
-                }
-
-                if (i < 2){s += ":";}
-                timeDelta += s;
-
-            }
-
-            return timeDelta;
+            int ds = timeDelta % (24 * 3600);
+            int dh = std::floor(double(ds) / 3600);
+            ds %= 3600;
             
-        }
+            int dm = std::floor(double(ds) / 60);
+            ds %= 60;
+
+            std::string stringTimeDelta {""};
+            stringTimeDelta += (std::to_string(dh) + " h ");
+            stringTimeDelta += (std::to_string(dm) + " min ");
+            stringTimeDelta += (std::to_string(ds) + " s ");
+
+            return stringTimeDelta;
+        
+    }
 };
 
 
 int main(){
 
-    std::string lol {"sasa:asdf"};
-    lol.
+    std::vector<MyTime> MytimeVec;
+        
+    std::cout << "This program calculates abs time-diff" << "\n";
+    for (size_t t = 0; t < 2; t++){
+        std::cout << "\n" << t + 1 << " moment of time:" << "\n";
 
+        MyTime mytime {};
+        for (size_t i = 0; i < 3; i++){
+            switch (i){
+                case 0:
+                    std::cout << "Give hour (0-24): ";
+                    std::cin >> mytime.hour;
+                    break;
+                case 1:
+                    std::cout << "Give minute (0-59): ";
+                    std::cin >> mytime.minute;
+                    break;
+                case 2:
+                    std::cout << "Give second (0-59): ";
+                    std::cin >> mytime.second;
+                    break;
+                default: break;
+            }
+        }
+        MytimeVec.push_back(mytime);
+    }
+    
+    MyTime t1 {MytimeVec[0]};
+    MyTime t2 {MytimeVec[1]};
 
-/*     MyTime t1 {14, 0, 4};
-    MyTime t2 {15, 50, 0};
-
-    std::cout << t2.getTimeDelta(t1);
+    std::cout << "Time difference is: " << t2.getTimeDelta(t1) << "\n\n";
 
     return 0;
-} */
+}
 
 // todo kesken
