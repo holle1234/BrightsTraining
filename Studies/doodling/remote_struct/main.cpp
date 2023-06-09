b8d66edd7034e8380e4fc69de7e7ad03b66e95c5
holle1234 <holle1234@github.com>
2023-06-09 19:09:10 +0300

    info added

--- Studies/doodling/remote_struct/main.cpp
+++ Studies/doodling/remote_struct/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include "header.h"
 
+
+// Showcase program for multifile projects
+// This program illustrates how struct member functions are defined in remote cpp.file
+
 int main(int argc, char const *argv[])
 {
     MyStruct st;
