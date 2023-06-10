dd048497708e287a887b378779b12c3cdabcbbce
holle1234 <holle1234@github.com>
2023-06-10 13:20:14 +0300

    separate folders for .h and .cpp

---
+++ Studies/doodling/remote_struct_circular_ref/cpps/cppfile.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <iostream>
+#include "headers/header.h"
+
+
+
+void MyStruct::structFunc(std::string input){
+    std::cout << "Hello from MyStruct! arg: " << input << "\n";
+}
+
---
+++ Studies/doodling/remote_struct_circular_ref/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "headers/header.h"
+
+
+// Showcase program for multifile projects
+// This program illustrates how struct member functions are defined in remote cpp.file
+
+int main(int argc, char const *argv[])
+{
+    MyStruct st;
+    st.structFunc("with love from main");
+
+    return 0;
+}
