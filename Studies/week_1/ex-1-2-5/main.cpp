ac13161e4df1b0c6bd3a02150f86779b82d7da9f
holle1234 <holle1234@github.com>
2023-06-21 10:40:16 +0300

    add books feat. added

--- Studies/week_1/ex-1-2-5/main.cpp
+++ Studies/week_1/ex-1-2-5/main.cpp
@@ -2,7 +2,11 @@
 #include <algorithm>
 #include <vector>
 #include <string>
+#include <iterator>
+#include <limits>
 
+
+struct Library;
 
 struct Book
 {
@@ -14,21 +18,97 @@ struct Book
 };
 
 
-std::ostream& std::operator<<(ostream& os, const Book& book)
+std::ostream& operator<<(std::ostream& os, const Book& book)
 {
-    return os << "book";
+    return os << "Book info: " << book.author << " " << book.title << " "
+              << book.year << " " << book.ISBN << " " << book.quantity << "\n";
 }
 
 
-struct Library
-{
+int convertToInt(std::string s){
+    try{
+        return std::stoi(s);
+    }
+    catch(const std::invalid_argument& e){
+        return std::numeric_limits<int>::quiet_NaN();
+    }
+}
+
+
+bool safeAssign(std::string& member, std::string& value){
+    if (!std::empty(value)){
+        member = value;
+        return true;
+    }
+    std::cout << "Something went wrong while assigning a member\n";
+    return false;
+}
+
+
+bool addBookDialog(Book& book){
+
+    std::string tempString;
+    std::cout << "Give book info\n";
+
+    std::cout << "Book title: ";
+    std::getline(std::cin, tempString);
+    if (!safeAssign(book.title, tempString)){
+        return false;
+    }
+
+    std::cout << "Book author: ";
+    std::getline(std::cin, tempString);
+    if (!safeAssign(book.author, tempString)){
+        return false;
+    }
+
+    std::cout << "Book year: ";
+    std::getline(std::cin, tempString);
+    if (!safeAssign(book.year, tempString)){
+        return false;
+    }
+
+    std::cout << "Book ISBN: ";
+    std::getline(std::cin, tempString);
+    if (!safeAssign(book.ISBN, tempString)){
+        return false;
+    }
+
+    std::cout << "Book quantity: ";
+    std::getline(std::cin, tempString);
+
+    int quantity = convertToInt(tempString);
+    if (quantity == std::numeric_limits<int>::quiet_NaN()){
+        return false;
+    }else {
+        book.quantity = quantity;
+    }
+    
+    return true;
+}
+
+
+struct Library{
+
     private:
         std::vector<Book> books;
         std::vector<Book> loaned;
     public:
+        Book getBookwithISBN(std::string ISBN){
+            for (auto &&book : books){
+                if (book.ISBN == ISBN){
+                    return book;
+                }
+            }
+            Book book;
+            return book;
+        }
+
         // add new book to library
-        void add(Book book){
-            books.push_back(book);
+        void add(){
+            Book newBook;
+            if (addBookDialog(newBook)){books.push_back(newBook);}
+            std::cout << "Something went wrong adding a book!\n";
         }
 
         // remove if exists
@@ -40,20 +120,32 @@ struct Library
             }
         }
         // print book via overloaded cout
-        void show(Book book){
+        void showBook(Book book){
             std::cout << book;
         }
 
-        Book loan(Book book){
-            auto it = std::find_if(loaned.begin(), loaned.end(),
-                      [&](const Book& b){return b.ISBN == book.ISBN;});
-            if (it == loaned.end()){
-                loaned.push_back(book);
+        // print all books
+        void showBooks(){
+            for (auto &&book : books){
+                showBook(book);
             }
         }
 
+        void loan(Book book){
+            auto it = std::find_if(loaned.begin(), loaned.end(),
+                      [&](const Book& b){return b.ISBN == book.ISBN;});
+
+            // not in loaned, add one with count 1
+            if (it == loaned.end()){
+                book.quantity = 1;
+                loaned.push_back(book);
+            }
+            // one loaned already add count
+            (*it).quantity += 1;
+        }
+
         // returns if book is in loaned vector
-        bool isLoaned(Book book){
+        bool isLoaned(const Book& book){
             auto it = std::find_if(loaned.begin(), loaned.end(),
                         [&](const Book& b){return b.ISBN == book.ISBN;});
             
@@ -79,7 +171,25 @@ see if the book has been loaned or not.
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+
+    Library library;
+    std::string cont;
+
+    while (true){
+
+        library.add();
+        std::cout << "Continue? (y/n): ";
+        std::cin >> cont;
+        std::cin.ignore(1);
+        if(cont == "n"){
+            break;
+        }
+    }
+
+    library.showBooks();
+    
+
+
     return 0;
 }
 
