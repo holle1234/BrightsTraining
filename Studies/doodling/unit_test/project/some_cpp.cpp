198777c79c71c79708a62bd01f3141a5750dfb58
holle1234 <holle1234@github.com>
2023-06-21 13:13:06 +0300

    unit testing

---
+++ Studies/doodling/unit_test/project/some_cpp.cpp
@@ -0,0 +1,11 @@
+#include "some_cpp.h"
+
+int sum_wrong(int a, int b)
+{
+    return a - b;
+}
+
+int sum_right(int a, int b){
+    return a + b;
+}
+
---
+++ Studies/doodling/unit_test/project/some_cpp.h
@@ -0,0 +1,2 @@
+int sum_wrong(int a, int b);
+int sum_right(int a, int b);
